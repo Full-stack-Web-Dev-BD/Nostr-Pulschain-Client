71adf81c835e3a1c182392690c6746395a9bacc5
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2024-04-02 18:22:28 +0600

    Profile page done - Balance reminig

--- src/views/profile/Profile.js
+++ src/views/profile/Profile.js
@@ -4,6 +4,9 @@ import CIcon from '@coreui/icons-react'
 import { CFormInput, CFormLabel } from '@coreui/react'
 import { jwtDecode } from 'jwt-decode'
 import Web3 from 'web3'
+import CopyToClipboard from 'react-copy-to-clipboard'
+import { toast } from 'react-toastify'
+import QRCode from 'react-qr-code'
 
 const Profile = () => {
   const [isShowKeys, setisShowKeys] = useState(false)
@@ -28,15 +31,33 @@ const Profile = () => {
       setPrivateKeys({ ...privateKeys, wsec, nsec: userInfo.nsec })
     }
   }
+
+  function shortenString(fullString) {
+    if (fullString) {
+      if (fullString.length <= 6) {
+        return fullString // If the string is 6 characters or less, return it as it is
+      }
+
+      const firstChars = fullString.slice(0, 15)
+      const lastChars = fullString.slice(-15)
+
+      return `${firstChars}...${lastChars}`
+    } else return ''
+  }
+
   return (
     <div>
       <div className="col-12 col-md-8 offset-md-2">
         <div className="profile_cover_wrap">
           <div className="profile_cover_photo">
             <button className="btn btn_cp_upload">
-              {/* <CIcon icon={cilCloudUpload} size="lg" /> */}
+              <CIcon icon={cilCloudUpload} size="lg" />
               <span>Upload</span>
             </button>
+            <h3 className="mt-5">
+              Your balance : <span>0.0003PLS</span>
+            </h3>
+
             <div className="profile_main_photo"></div>
           </div>
         </div>
@@ -55,25 +76,40 @@ const Profile = () => {
                   <h3 className="mt-5">Public Keys</h3>
                   <hr />
                   <div className="web3pub_qr_code text-center mt-4 mb-4">
-                    <img src="/img/qr.png" />
+                    {userInfo.wpub ? (
+                      <div className="qr_wrap">
+                        <QRCode
+                          size={256}
+                          style={{ height: 'auto', maxWidth: '300px', width: '300px' }}
+                          value={userInfo.wpub}
+                          viewBox={`0 0 256 256`}
+                        />
+                      </div>
+                    ) : (
+                      ''
+                    )}
                     <p className="mt-2"> Your Web3 Wallet </p>
                   </div>
                   <div className="mb-3 color_keys">
                     <CFormLabel htmlFor="exampleFormControlInput1">Nostr Public Key</CFormLabel>
                     <p>
-                      <strong>{userInfo.npub}</strong>
-                      <span>
-                        {/* <CIcon icon={cilCopy} size="lg" /> */}
-                      </span>
+                      <strong>{shortenString(userInfo.npub)}</strong>
+                      <CopyToClipboard text={userInfo.npub}>
+                        <span onClick={(e) => toast('Copied To Clipboard!')}>
+                          <CIcon icon={cilCopy} size="lg" />
+                        </span>
+                      </CopyToClipboard>
                     </p>
                   </div>
                   <div className="mb-3 color_keys">
                     <CFormLabel htmlFor="exampleFormControlInput1">Web3 Public Key</CFormLabel>
                     <p>
-                      <strong> {userInfo.wpub} </strong>
-                      <span>
-                        {/* <CIcon icon={cilCopy} size="lg" /> */}
-                      </span>
+                      <strong> {shortenString(userInfo.wpub)} </strong>
+                      <CopyToClipboard text={userInfo.wpub}>
+                        <span onClick={(e) => toast('Copied To Clipboard!')}>
+                          <CIcon icon={cilCopy} size="lg" />
+                        </span>
+                      </CopyToClipboard>
                     </p>
                   </div>
                 </div>
@@ -91,29 +127,49 @@ const Profile = () => {
                           Nostr Private Key
                         </CFormLabel>
                         <p>
-                          <strong>
-                            {privateKeys.nsec}
-                          </strong>
-                          <span>
-                            {/* <CIcon icon={cilCopy} size="lg" /> */}
-                          </span>
+                          <strong>{shortenString(privateKeys.nsec)}</strong>
+                          <CopyToClipboard text={userInfo.nsec}>
+                            <span onClick={(e) => toast('Copied To Clipboard!')}>
+                              <CIcon icon={cilCopy} size="lg" />
+                            </span>
+                          </CopyToClipboard>
                         </p>
                       </div>
                       <div className="mb-3 color_keys">
                         <CFormLabel htmlFor="exampleFormControlInput1">Web3 Private Key</CFormLabel>
                         <p>
-                          <strong>
-                          {privateKeys.wsec}
-                          </strong>
-                          <span>
-                            {/* <CIcon icon={cilCopy} size="lg" /> */}
-                          </span>
+                          <strong>{shortenString(privateKeys.wsec)}</strong>
+                          <CopyToClipboard text={userInfo.wsec}>
+                            <span onClick={(e) => toast('Copied To Clipboard!')}>
+                              <CIcon icon={cilCopy} size="lg" />
+                            </span>
+                          </CopyToClipboard>
                         </p>
                       </div>
                       <div></div>
                     </div>
                   ) : (
-                    ''
+                    <div>
+                      <div className="mb-3 color_keys">
+                        <CFormLabel htmlFor="exampleFormControlInput1">
+                          Nostr Private Key
+                        </CFormLabel>
+                        <p>
+                          <strong className="password_line">
+                            {shortenString(privateKeys.nsec)}
+                          </strong>
+                        </p>
+                      </div>
+                      <div className="mb-3 color_keys">
+                        <CFormLabel htmlFor="exampleFormControlInput1">Web3 Private Key</CFormLabel>
+                        <p>
+                          <strong className="password_line">
+                            {shortenString(privateKeys.wsec)}
+                          </strong>
+                        </p>
+                      </div>
+                      <div></div>
+                    </div>
                   )}
                 </div>
               </div>
