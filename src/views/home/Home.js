59dcf826f9262afee6043c93d7a92d6c5f9e6b28
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2024-04-05 14:52:56 +0600

    home page content showing done

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -10,7 +10,7 @@ import {
 import { useDispatch, useSelector } from 'react-redux'
 import CIcon from '@coreui/icons-react'
 import { Link } from 'react-router-dom'
-import { createNote, fileUpload } from '../../utils/function'
+import { createNote, extractTextAndImage, fileUpload, shortenString } from '../../utils/function'
 import LoadingContent from '../pages/Loading/LoadingContent'
 
 const Home = () => {
@@ -121,87 +121,93 @@ const Home = () => {
 
       <div className="row">
         <div className="col-12 col-md-8 offset-md-2">
-          {
-            userState.stockEvents.length <1 ? 
+          {userState.stockEvents.length < 1 ? (
             <>
-            <LoadingContent/>
+              <LoadingContent />
             </>
-            :
-          <div className="card mb-4">
-            <div className="card-body">
-              {userState.stockEvents.map((note) => (
-                <div className="col-sm-11 ms-auto mt-5" key={note.id}>
-                  <div className="user_profile_box user_profile_link">
-                    <img src="/img/8.jpg" />
-                    <div>
-                      <Link to={'#'}>
-                        <h5> User Name </h5>
-                      </Link>
+          ) : (
+            <div className="card mb-4">
+              <div className="card-body">
+                {userState.stockEvents.map((note) => (
+                  <div className="col-sm-11 ms-auto mt-5" key={note.id}>
+                    <div className="user_profile_box user_profile_link">
+                      <img src="/img/8.jpg" />
+                      <div>
+                        <Link to={'#'}>
+                          <h5> User Name </h5>
+                        </Link>
 
-                      <p>
-                        PubKey : <strong> {note.pubkey} </strong>
-                        <span>
-                          <CIcon icon={cilCopy} size="lg" />
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="mb-3 mt-4 post_content">
-                    <p> I recently started Nostr to share my thought </p>
-                    <img src="/img/post.jpg" />
-                  </div>
-                  <div>
-                    <div className="upload_panal">
-                      <div className="post_reaction_item">
-                        <span className="reaction_icon">
-                          <CIcon icon={cilChatBubble} size="xl" />
-                        </span>
-                        <span>0</span>
-                      </div>
-                      <div className="post_reaction_item">
-                        <span className="reaction_icon">
-                          <CIcon icon={cilSwapHorizontal} size="xl" />
-                        </span>
-                        <span>0</span>
-                      </div>
-                      <div className="post_reaction_item">
-                        <span className="reaction_icon">
-                          <CIcon icon={cilHeart} size="xl" />
-                        </span>
-                        <span>0</span>
+                        <p>
+                          PubKey : <strong> {shortenString(note.pubkey)} </strong>
+                          <span>
+                            <CIcon icon={cilCopy} size="lg" />
+                          </span>
+                        </p>
                       </div>
                     </div>
-                  </div>
-                  <div>
-                    <div className="card mb-4">
-                      <div className="card-body">
-                        <textarea
-                          className="form-control no_border"
-                          id="exampleFormControlTextarea1"
-                          rows="3"
-                          placeholder="Wnat's in your mind ?"
-                          spellCheck="false"
-                        ></textarea>
-                        <div className="post_comments">
-                          {[1, 2, 3, 4, 5].map((el) => (
-                            <div className="comment_user mb-4" key={el}>
-                              <img src="/img/8.jpg" />
-                              <div>
-                                <h6>Alamin Hossin</h6>
-                                <p> Nice , its a great plateform to use ~ </p>
-                              </div>
-                            </div>
-                          ))}
+                    <div className="mb-3 mt-4 post_content">
+                      {extractTextAndImage(note.content).text ? (
+                        <p> {extractTextAndImage(note.content).text} </p>
+                      ) : (
+                        ''
+                      )}
+                      {extractTextAndImage(note.content).img ? (
+                        <img src={extractTextAndImage(note.content).img} />
+                      ) : (
+                        ''
+                      )}
+                    </div>
+                    <div>
+                      <div className="upload_panal">
+                        {/* <div className="post_reaction_item">
+                          <span className="reaction_icon">
+                            <CIcon icon={cilChatBubble} size="xl" />
+                          </span>
+                          <span>0</span>
+                        </div> */}
+                        {/* <div className="post_reaction_item">
+                          <span className="reaction_icon">
+                            <CIcon icon={cilSwapHorizontal} size="xl" />
+                          </span>
+                          <span>0</span>
+                        </div> */}
+                        <div className="post_reaction_item">
+                          <span className="reaction_icon">
+                            <CIcon icon={cilHeart} size="xl" />
+                          </span>
+                          <span> {note.tags.length} </span>
                         </div>
                       </div>
                     </div>
+                    {/* <div>
+                      <div className="card mb-4">
+                        <div className="card-body">
+                          <textarea
+                            className="form-control no_border"
+                            id="exampleFormControlTextarea1"
+                            rows="3"
+                            placeholder="Wnat's in your mind ?"
+                            spellCheck="false"
+                          ></textarea>
+                          <div className="post_comments">
+                            {[1, 2, 3, 4, 5].map((el) => (
+                              <div className="comment_user mb-4" key={el}>
+                                <img src="/img/8.jpg" />
+                                <div>
+                                  <h6>Alamin Hossin</h6>
+                                  <p> Nice , its a great plateform to use ~ </p>
+                                </div>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </div>
+                    </div> */}
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-          }
-
+          )}
         </div>
       </div>
     </div>
